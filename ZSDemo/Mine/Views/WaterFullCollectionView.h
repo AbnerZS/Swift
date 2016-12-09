62fb4b4cefa78c85b16aa610780f62707954b392
AbnerZhang <zhangshuo@zhangshuodeMacBook-Pro.local>
2016-12-09 10:23:52 +0800

    about project framework

---
+++ ZSDemo/Mine/Views/WaterFullCollectionView.h
@@ -0,0 +1,16 @@
+//
+//  WaterFullCollectionView.h
+//  ZSDemo
+//
+//  Created by AbnerZhang on 2016/12/1.
+//  Copyright © 2016年 abnerzhang. All rights reserved.
+//
+
+#import "BaseCollectionView.h"
+
+@interface WaterFullCollectionView : BaseCollectionView
+
+@property (nonatomic, strong) NSMutableArray *dataArray;
+
+
+@end
---
+++ ZSDemo/Mine/Views/WaterFullCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  WaterFullCollectionViewCell.h
+//  ZSDemo
+//
+//  Created by AbnerZhang on 2016/12/1.
+//  Copyright © 2016年 abnerzhang. All rights reserved.
+//
+
+#import "BaseCollectionView.h"
+
+@interface WaterFullCollectionViewCell : BaseCollectionView
+
+@end
