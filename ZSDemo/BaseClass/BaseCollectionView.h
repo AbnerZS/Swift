60c70c00795993ee2f4af6f76cc31de08c279728
AbnerZhang <zhangshuo@zhangshuodeMacBook-Pro.local>
2016-12-09 11:07:19 +0800

    about project framework

---
+++ ZSDemo/BaseClass/BaseCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  BaseCollectionView.h
+//  ZSDemo
+//
+//  Created by AbnerZhang on 2016/12/1.
+//  Copyright © 2016年 abnerzhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseCollectionView : UICollectionView
+
+@end
---
+++ ZSDemo/BaseClass/BaseCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  BaseCollectionViewCell.m
+//  ZSDemo
+//
+//  Created by AbnerZhang on 2016/12/1.
+//  Copyright © 2016年 abnerzhang. All rights reserved.
+//
+
+#import "BaseCollectionViewCell.h"
+
+@implementation BaseCollectionViewCell
+
+@end
